71555650e16909b7840bd21d6f596f64839c9ed5
shubham sharma <shubhamgetmail@gmail.com>
2024-08-27 23:04:25 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -4,7 +4,25 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  // socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    // convert data to string
+    const request = data.toString();
+    console.log("Recieved request: ", request);
+
+    // extract the URL path from the request
+    const requestLine = request.split("\r\n")[0];
+    const [method, path, version] = requestLine.split(" ");
+
+    // send the response
+    if(path === "/"){
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    }
+    else{
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+    socket.end();
+  });
   socket.on("close", () => {
     socket.end();
   });
