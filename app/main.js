07497db6da1d924cbf0223b1e2893d773b2662a8
shubham sharma <shubhamgetmail@gmail.com>
2024-08-28 19:12:47 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,4 +1,7 @@
 const net = require("net");
+const fs = require("fs");
+const path = require("path");
+const { Socket } = require("dgram");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -14,6 +17,9 @@ const server = net.createServer((socket) => {
     const url = request.split(" ")[1]; // accessing the {str} directly
     const headers = request.split("\r\n");
 
+    //filepath
+    let filepath = '';
+
     // send the response
     if(url === "/"){
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
@@ -28,6 +34,31 @@ const server = net.createServer((socket) => {
     else if(url === '/user-agent'){
       const userAgent = headers[2].split("User-Agent: ")[1];
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`); 
+    }
+    else if(url.includes('/files/')){
+      const filname = url.split('/files/')[1];
+      filepath = path.join(__dirname, filname);
+
+      // check if file exists
+      fs.stat(filepath, (err, stat) => {
+        if(err || !stat.isFile()){
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+        else{
+          // File exist
+          //read File
+          fs.readFile(filepath, (err, fileData) => {
+            if(err){
+              socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+            }
+            else{
+              socket.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileData.length}");
+              socket.write(fileData);
+            }
+          })
+        }
+      });
+
     }
     else{
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
